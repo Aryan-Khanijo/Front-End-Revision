2fc6051ff546b1a4c4cf8143126a1954fc0b66ad
aryancooldude <akhanijo12345@gmail.com>
2020-07-02 00:53:14 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Front-End-Revision
+Waifu XP
---
+++ script.js
@@ -0,0 +1,13 @@
+var x = 0;
+var namelist = ['Akeno','Kojima','Megumin','Hinata','Boa'];
+function changePicnName()
+{
+    if (x == 5)
+    {
+        x = 0;
+    }
+    var img = "url('media/" + namelist[x] +".jpg')";
+    document.getElementById('waifu').style.backgroundImage = img ;
+    document.getElementById('name').innerHTML = namelist[x];
+    x += 1;
+}
